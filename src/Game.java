bd49f492075e3197b1d2b134861a42639580b130
kush018 <64299526+kush018@users.noreply.github.com>
2020-12-25 17:44:28 +0530

    converted all byte arrays to int; added clickTile method; added checkWon method; added display method

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,8 @@
 public class Game {
     //gameState stores all user actions (visible to user)
-    private byte[][] gameState;
+    private int[][] gameState;
     //gameBoard stores positions of all mines and other data (invisible to user)
-    private byte[][] gameBoard;
+    private int[][] gameBoard;
 
     //if the space has a mine (for gameBoard only)
     private static final int MINE = -1;
@@ -18,8 +18,8 @@ public class Game {
         //q - length of the game board
         //mines - no. of mines
 
-        gameState = new byte[p][q];
-        gameBoard = new byte[p][q];
+        gameState = new int[p][q];
+        gameBoard = new int[p][q];
 
         /*
         q ------------->
@@ -63,7 +63,7 @@ public class Game {
             for (int j = 0; j < gameBoard[i].length; j++) {
                 if (!(gameBoard[i][j] == MINE)) {
                     //if the thing is not a mine
-                    gameBoard[i][j] = (byte) findMines(i, j);
+                    gameBoard[i][j] = findMines(i, j);
                 }
             }
         }
@@ -122,7 +122,85 @@ public class Game {
         }
     }
 
-    public static int clickTile(int p, int q, int operation) {
-        return 0;
+    public static final int ALL_FINE = 0;
+    public static final int ALREADY_CLICKED = 1;
+    public static final int INVALID_POSITION = -1;
+    public static final int PLAYER_LOST = -2;
+    public static final int PLAYER_WON = 2;
+    public static final int ALREADY_FLAGGED = -3;
+
+    public int clickTile(int p, int q) {
+        try {
+            if (gameState[p][q] == NULL) {
+                if (gameBoard[p][q] != MINE) {
+                    //if not clicked on mine
+                    gameState[p][q] = CLICKED;
+                    if (gameBoard[p][q] == 0) {
+                        //zero mines surround clicked area
+                        clickTile(p - 1, q - 1);
+                        clickTile(p - 1, q);
+                        clickTile(p - 1, q + 1);
+                        clickTile(p, q + 1);
+                        clickTile(p + 1, q + 1);
+                        clickTile(p + 1, q);
+                        clickTile(p + 1, q - 1);
+                        clickTile(p, q - 1);
+                    }
+                    if (checkWon()) {
+                        return PLAYER_WON;
+                    } else {
+                        return ALL_FINE;
+                    }
+                } else {
+                    //clicked on a mine
+                    return PLAYER_LOST;
+                }
+            } else if (gameState[p][q] == FLAGGED) {
+                return ALREADY_FLAGGED;
+            } else {
+                return ALREADY_CLICKED;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return INVALID_POSITION;
+        }
+    }
+
+    public boolean checkWon() {
+        for (int i = 0; i < gameState.length; i++) {
+            for (int j = 0; j < gameState[i].length; j++) {
+                if ((gameState[i][j] == NULL || gameState[i][j] == FLAGGED) && gameBoard[i][j] != MINE) {
+                    //if the positions i and j are not clicked and they also do not have a mine
+                    //i.e a position i and j exists that is not clicked and not mined
+                    return false;
+                }
+            }
+        }
+        //this means all the positions that are not clicked are mined
+        return true;
+    }
+
+    public void display() {
+        System.out.print("  q  ");
+        for (int i = 0; i < gameState[0].length; i++) {
+            System.out.print(i + "   ");
+        }
+        System.out.println();
+        for (int i = 0; i < gameState.length; i++) {
+            for (int j = 0; j < gameState[i].length; j++) {
+                if (i == 0) {
+                    System.out.print("p 0 ");
+                } else {
+                    System.out.print("  " + i + " ");
+                }
+                if (gameState[i][j] == NULL) {
+                    System.out.print("[_] ");
+                } else if (gameState[i][j] == CLICKED) {
+                    System.out.print("{" + gameBoard[i][j] + "} ");
+                } else if (gameState[i][j] == FLAGGED) {
+                    System.out.print("(F) ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
